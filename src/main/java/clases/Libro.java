755a8e8192db44fda0db5e169278c92e32aed1d0
José Pablo Machuca González <71435599+JPPurpleheart@users.noreply.github.com>
2021-06-04 10:26:59 +0200

    Initial commit

---
+++ src/main/java/clases/Libro.java
@@ -0,0 +1,16 @@
+package clases;
+
+public class Libro {
+	
+	private String titulo;
+	private String isbn;
+	private Genero genero;
+	private String autor;
+	private Integer paginas;
+	
+	public Libro() {
+		
+	}
+	
+
+}
